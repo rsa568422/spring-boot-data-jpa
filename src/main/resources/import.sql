d1f92ece96012648ad88d718aaa8b55040202b64
Roberto Sánchez Arévalo <rsa568422@gmail.com>
2021-10-03 17:50:25 +0200

    Clase 116
    Se agrega el fichero import.sql con los datos de prueba a insertar en la
    base de datos.
    
    Se agrega la vista list.html para mostrar el listado de clientes.

---
+++ src/main/resources/import.sql
@@ -0,0 +1,2 @@
+INSERT INTO clients (id, name, surname, email, create_at) VALUES (1, 'Andrés', 'Guzmán', 'andre@email.com', '2021-01-01');
+INSERT INTO clients (id, name, surname, email, create_at) VALUES (2, 'Roberto', 'Sánchez', 'roberto@email.com', '2021-01-02');
