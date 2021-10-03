466e8fa7627a886ca6d4b0729884b1131a35c9f6
Roberto Sánchez Arévalo <rsa568422@hotmail.com>
2021-10-03 22:18:52 +0200

    Clase 120
    Se agrega el método prePersist a la clase Client etiquetado con
    @PrePersist, lo que hace que se ejecute antes de ejecutarse el método
    persist.
    
    Se agrega la vista form.html con el formulario para la inserción de un
    cliente.
    
    Se agrega link a la nueva vista en list.html.

--- src/main/java/com/bolsadeideas/springboot/app/models/entity/Client.java
+++ src/main/java/com/bolsadeideas/springboot/app/models/entity/Client.java
@@ -8,6 +8,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.PrePersist;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
@@ -38,5 +39,10 @@ public class Client implements Serializable {
 	@Column(name = "create_at")
 	@Temporal(TemporalType.DATE)
 	private Date createAt;
+	
+	@PrePersist
+	public void prePersist() {
+		this.createAt = new Date();
+	}
 
 }
