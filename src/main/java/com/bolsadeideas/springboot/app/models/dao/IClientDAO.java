965a8e39e7c3582bc9ba6bc483ecabcf4546afd1
Roberto Sánchez Arévalo <rsa568422@hotmail.com>
2021-10-05 01:44:45 +0200

    Clase 141
    Versión original del proyecto una vez finalizada la sección 9.
    
    NOTA: no se realizará el merge con develop para conservar la versión personalizada del proyecto.

--- src/main/java/com/bolsadeideas/springboot/app/models/dao/IClientDAO.java
+++
@@ -1,9 +0,0 @@
-package com.bolsadeideas.springboot.app.models.dao;
-
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-import com.bolsadeideas.springboot.app.models.entity.Client;
-
-public interface IClientDAO extends PagingAndSortingRepository<Client, Long> {
-
-}
