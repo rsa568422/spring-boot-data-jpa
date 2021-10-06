7e2b0d5a069ec620291cc455206d6c939008a105
Roberto Sánchez Arévalo <rsa568422@hotmail.com>
2021-10-06 22:00:58 +0200

    Clase 156
    Se agrega la clase Receipt que representa la tabla receipts. Se
    establece una relación con la tabla clients mediante el atributo client
    etiquetado como @ManyToOne.
    
    Se agrega el atributo receipts etiquetado con @OneToMany a la clase
    Client para indicar su relacion con la tabla receipts.
    
    NOTA: Actualización del UML.

--- src/main/java/com/bolsadeideas/springboot/app/models/entity/Receipt.java
+++ src/main/java/com/bolsadeideas/springboot/app/models/entity/Receipt.java
@@ -1,9 +1,7 @@
 package com.bolsadeideas.springboot.app.models.entity;
 
 import java.io.Serializable;
-import java.util.ArrayList;
 import java.util.Date;
-import java.util.List;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
@@ -12,26 +10,22 @@ import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.PrePersist;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
-
-import org.springframework.format.annotation.DateTimeFormat;
 
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity
-@Table(name = "clients")
+@Table(name = "receipts")
 @Getter
 @Setter
 @EqualsAndHashCode
-public class Client implements Serializable {
+public class Receipt implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
@@ -39,33 +33,20 @@ public class Client implements Serializable {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
-	@NotEmpty
-	private String name;
+	private String description;
 
-	@NotEmpty
-	private String surname;
+	private String observations;
 
-	@Email
-	@NotEmpty
-	private String email;
-
-	@NotNull
 	@Column(name = "create_at")
 	@Temporal(TemporalType.DATE)
-	@DateTimeFormat(pattern = "yyyy-MM-dd")
 	private Date createAt;
-	
-	@OneToMany(mappedBy = "client", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
-	private List<Receipt> receipts;
-	
-	private String photo;
-	
-	public Client() {
-		this.receipts = new ArrayList<>();
-	}
-	
-	public void addReceipt(Receipt receipt) {
-		this.receipts.add(receipt);		
+
+	@ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+	private Client client;
+
+	@PrePersist
+	public void prePersist() {
+		this.createAt = new Date();
 	}
 
 }
